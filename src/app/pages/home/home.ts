c455491a9f91dedc64fe680d60a4ffdfafcde200
Yeiner Daniel Molina Blanco <yeinermoli@gmail.com>
2025-10-06 18:04:34 -0500

    First Commit

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { RippleModule } from 'primeng/ripple';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ButtonModule, CardModule, InputTextModule, RippleModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
